46afb48e56dc80112016b3f05e7a83514f9e49d5
huyen <huyenb1910648@example.com>
2022-11-29 15:26:30 +0700

    Update frontend

---
+++ contactbook-frontend/src/router/index.js
@@ -0,0 +1,37 @@
+import { createWebHistory, createRouter } from 'vue-router';
+import ContactBook from '@/views/ContactBook.vue';
+const routes = [{
+        path: '/',
+        name: 'contactbook',
+        component: ContactBook,
+    },
+    {
+        path: '/contacts/:id',
+        name: 'contact.edit',
+        component: () =>
+            import ('@/views/ContactEdit.vue'),
+        props: (route) => ({ contactId: parseInt(route.params.id) })
+    },
+    {
+        path: '/contacts/add',
+        name: 'contact.add',
+        component: () =>
+            import ('@/views/ContactAdd.vue'),
+        props: (route) => ({ contact: route.param })
+    },
+    {
+        path: '/:pathMatch(.*)*',
+        name: 'notfound',
+        component: () =>
+            import ('@/views/NotFound.vue'),
+    },
+
+
+
+];
+const router = createRouter({
+    history: createWebHistory(
+        import.meta.env.BASE_URL),
+    routes,
+});
+export default router;
